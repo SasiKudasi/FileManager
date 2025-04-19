67f80b28b5c8fa9744e438e8ea5316ae82e6a8fa
SasiKudasi <sasha.mittal@yandex.ru>
2025-04-19 13:48:27 +0300

    upd ls cmd

--- src/main/java/ru/filemanager/commands/ListCommand.java
+++ src/main/java/ru/filemanager/commands/ListCommand.java
@@ -7,18 +7,22 @@ import java.util.Date;
 import java.util.Objects;
 
 public class ListCommand implements Command {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Override
     public void execute(String[] args) {
         Path pwd = Path.of(args[0]);
         if (pwd.toFile().isDirectory()) {
-            System.out.printf("%-30s %-5s %-20s%n", "NAME", "TYPE", "LAST MODIFIED");
-            System.out.println("=".repeat(60));
+            System.out.printf("%-30s %-5s %-5s %-5s %-20s%n", "NAME", "TYPE", "READ", "WRITE", "LAST MODIFIED");
+            System.out.println("=".repeat(70));
             for (File item : Objects.requireNonNull(pwd.toFile().listFiles())) {
                 char type = item.isDirectory() ? 'D' : 'F';
                 String formattedDate = sdf.format(new Date(item.lastModified()));
-                System.out.printf("%-30s %-5s %-20s%n",  item.getName(),  type,  formattedDate);
+                char read = item.canRead() ? 'R' : ' ';
+                char write = item.canWrite() ? 'W' : ' ';
+
+
+                System.out.printf("%-30s %-5s %-5s %-5s %-20s%n",  item.getName(), type, read, write, formattedDate);
             }
         }
     }
