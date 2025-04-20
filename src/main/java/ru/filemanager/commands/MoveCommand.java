bbf2dd34ffb58dc904fa35be4f6cfa5ddf00cf79
SasiKudasi <sasha.mittal@yandex.ru>
2025-04-20 22:23:06 +0300

    now mv command work in current dir

--- src/main/java/ru/filemanager/commands/MoveCommand.java
+++ src/main/java/ru/filemanager/commands/MoveCommand.java
@@ -1,6 +1,9 @@
 package ru.filemanager.commands;
 
+import ru.filemanager.commands.util.PathContext;
+
 import java.io.File;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,17 +11,22 @@ public class MoveCommand implements Command {
 
     @Override
     public void execute(String[] args) {
+        Path currentDir = PathContext.getInstance().getCurrentDir().toPath();
 
-        File source = new File(args[0]);
+        Path sourcePath = currentDir.resolve(args[0]).normalize();
+        File source = sourcePath.toFile();
+
         if (!source.exists() || !source.isFile()) {
             System.out.println("it is not file");
             return;
         }
 
-        File targetDir = new File(args[1]);
+        Path targetPath = currentDir.resolve(args[1]).normalize();
+        File targetDir = targetPath.toFile();
+
         if (!targetDir.exists()) {
             if (!targetDir.mkdirs()) {
-                System.out.println("can not create dir");
+                System.out.println("cannot create dir");
                 return;
             }
         }
